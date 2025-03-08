64087978e2c55422c60761f9e3b8b6fecf2c48c5
Sebas <sebaslg96@gmail.com>
2025-03-08 00:42:49 -0500

    agregado readme.md y seteado pagina de error

--- Modules/Errors/error.controller.php
+++ Modules/Errors/error.controller.php
@@ -7,7 +7,9 @@ class Errors extends Controllers{
     public function notFound(){
         cm_page(array(
             'class' => $this,
-            'view' => 'error',
+            'page_title' => 'Error',
+            'page_id' => 'error',
+            'view' => 'errors',
         ));
     }
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Proyecto CRUD Master
+
+-Para tener en cuenta:
+Sistema de noticiaciones en tiempo real
+https://www.youtube.com/watch?v=zPGrqCB03Wo

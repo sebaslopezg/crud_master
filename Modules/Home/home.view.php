98c0a23e0d2f4d214768ccab8111bd6f052968c6
lulo <lopez.gallego2611@gmail.com>
2025-06-18 00:21:21 -0500

    landing base

--- Modules/Home/home.view.php
+++ Modules/Home/home.view.php
@@ -1,7 +1,110 @@
-<?php //header_admin($data); ?>
+<?php header_landing(); ?>
 <!--   <main id="main" class="main">
     
   </main> -->
-  home
-  <a href="<?= base_url() ?>/dashboard">Ingresar</a>
-  <?php //footer_admin($data); ?>
+<!--START PARALLAX SECTION-->
+<div class='parallax-wrapper'>
+
+  <!--INTRO SECTION-->
+  <div clas='parallax-group intro-screen'>
+    <section class='hidden'>
+      <h1 class='big-title'>CRUD master</h1>   
+      <p>algo aquí</p>
+    </section>
+  </div>
+
+<!--SECOND SECTION-->
+<div class='parallax-group'>
+  <div class='parallax-layer base-layer'>
+    <section></section>
+  </div>
+  <div class='parallax-layer mid-layer'>
+    <section class='hidden'>
+      <h1>Something else</h1>
+    </section>
+  </div>
+</div>
+<!--END SECOND SECTION-->
+
+<!--THIRD SECTION-->
+<div class='parallax-group'>
+  <div class='parallax-layer mid-layer'>
+    <section></section>
+  </div>
+  <div class='parallax-layer top-layer'>
+    <section class='bg-image'>
+    <section class=''>
+        <div class="container text-center d-grid gap-3">
+          <div class="row mx-auto">
+            <div class="col-md">
+            <div class="card card-bg text-center mb-3 mx-auto">
+            <div class="card-body text-center">
+              <h1><i class="bi bi-shop"></i></h1>
+              <h1>Gestión de tiendas</h1>
+            </div>
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="card card-bg text-center mb-3 mx-auto">
+            <div class="card-body text-center">
+              <h1><i class="bi bi-person-circle"></i></h1>
+              <h1>Manejo de usuarios</h1>
+            </div>
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="card card-bg text-center mb-3 mx-auto">
+            <div class="card-body text-center mx-auto">
+              <h1><i class="bi bi-box2-heart"></i></h1>
+              <h1>Productos e inventario</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row mx-auto">
+        <div class="col-md">
+          <div class="card card-bg text-center mb-3 mx-auto">
+            <div class="card-body text-center">
+              <h1><i class="bi bi-receipt"></i></h1>
+              <h1>Facturación y ventas</h1>
+            </div>
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="card card-bg text-center mb-3 mx-auto">
+            <div class="card-body text-center">
+              <h1><i class="bi bi-tags"></i></h1>
+              <h1>Sistema de roles</h1>
+            </div>
+          </div>
+        </div>
+          <div class="col-md">
+            <div class="card card-bg text-center mb-3 mx-auto">
+              <div class="card-body text-center">
+                <h1><i class="bi bi-pencil"></i></h1>
+                <h1>Personalización y adaptabilidad</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    </section>
+    </section>
+  </div>
+</div>
+<!--END THIRD SECTION-->
+
+<!--OUTRO SECTION-->
+<div class='parallax-section outro-screen'>
+<section class='hidden'>
+  <h1>And then, something else</h1>
+</section>
+</div>
+<!--END OUTRO SECTION-->
+
+<!--END INTRO SECTION-->
+</div>
+<!--END PARALLAX SECTION->
+
+  <?php footer_landing(); ?>
---
+++ Template/Landing/header_landing.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Crud Master</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  <link href="<?= media() ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= media() ?>/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <!-- Template Main CSS File -->
+  <link href="<?= media() ?>/css/landingStyle.css" rel="stylesheet"> 
+</head>
+
+<body>

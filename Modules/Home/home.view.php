6f456153fba7008349f1c27db9004b84e8996d04
lulo <lopez.gallego2611@gmail.com>
2025-06-19 02:06:43 -0500

    landingsita

--- Modules/Home/home.view.php
+++ Modules/Home/home.view.php
@@ -6,34 +6,37 @@
 <div class='parallax-wrapper'>
 
   <!--INTRO SECTION-->
-  <div clas='parallax-group intro-screen'>
+  <div class='parallax-section intro-screen' id='intro'>
     <section class='hidden'>
-      <h1 class='big-title'>CRUD master</h1>   
-      <p>algo aquí</p>
+      <h1 class='big-title'>CRUD Master</h1>   
+      <div class="container text-center d-grid gap-3">
+        <div class="row mx-auto">
+          <div class='col-md'>
+              <h1><i class="bi bi-shop"></i></h1>
+          </div>
+          <div class='col-md'>
+              <h1><i class="bi bi-person-circle"></i></h1>
+          </div>
+          <div class='col-md'>
+              <h1><i class="bi bi-box2-heart"></i></h1>
+          </div>
+                    <div class='col-md'>
+              <h1><i class="bi bi-receipt"></i></h1>
+          </div>
+          <div class='col-md'>
+              <h1><i class="bi bi-tags"></i></h1>
+          </div>
+          <div class='col-md'>
+              <h1><i class="bi bi-pencil"></i></h1>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 
 <!--SECOND SECTION-->
-<div class='parallax-group'>
-  <div class='parallax-layer base-layer'>
-    <section></section>
-  </div>
+<div class='parallax-group' id='group-1'>
   <div class='parallax-layer mid-layer'>
-    <section class='hidden'>
-      <h1>Something else</h1>
-    </section>
-  </div>
-</div>
-<!--END SECOND SECTION-->
-
-<!--THIRD SECTION-->
-<div class='parallax-group'>
-  <div class='parallax-layer mid-layer'>
-    <section></section>
-  </div>
-  <div class='parallax-layer top-layer'>
-    <section class='bg-image'>
-    <section class=''>
         <div class="container text-center d-grid gap-3">
           <div class="row mx-auto">
             <div class="col-md">
@@ -93,13 +96,91 @@
     </section>
   </div>
 </div>
+<!--END SECOND SECTION-->
+
+<!--THIRD SECTION-->
+<div class='parallax-group-small' id='group-2'>
+  <div class='parallax-layer base-layer'>
+    <section>
+      <div class="container">
+        <div class="d-flex align-items-center">
+          <div class='card-parent'>
+            <div class='card-child'></div>
+          </div>  
+            <div class='div-width'>
+              <div class="d-flex align-items-center">
+                <div class="icon-circle me-3">
+                  <i class="bi bi-shop circle-icon"></i>
+                </div>
+                <h1 class='title-color'>Gestión de tiendas</h1>
+              </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia eos optio qui voluptates quasi iste perferendis veritatis sequi libero dolorem ratione quidem, aut tempore doloremque nisi. Repellendus explicabo magnam perferendis.
+            Tenetur sapiente, repellat cumque dolorum, minus quas commodi et, provident odio corporis cum architecto. Alias provident consequatur amet ipsa tenetur nam dignissimos deleniti officia, autem vitae illum, impedit dolorem adipisci?</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
 <!--END THIRD SECTION-->
 
+<!--FORTH SECTION-->
+<div class='parallax-group-small' id='group-2'>
+  <div class='parallax-layer base-layer'>
+    <section>
+      <div class="container">
+        <div class="d-flex align-items-center">
+            <div class='div-width'>
+              <div class="d-flex align-items-center">
+                <div class="icon-circle me-3">
+                  <i class="bi bi-shop circle-icon"></i>
+                </div>
+                <h1 class='title-color'>Gestión de tiendas</h1>
+              </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia eos optio qui voluptates quasi iste perferendis veritatis sequi libero dolorem ratione quidem, aut tempore doloremque nisi. Repellendus explicabo magnam perferendis.
+            Tenetur sapiente, repellat cumque dolorum, minus quas commodi et, provident odio corporis cum architecto. Alias provident consequatur amet ipsa tenetur nam dignissimos deleniti officia, autem vitae illum, impedit dolorem adipisci?</p>
+          </div>
+          <div class='card-parent'>
+            <div class='card-child'></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+<!--END FORTH SECTION-->
+
+<!--FIFTH SECTION-->
+<div class='parallax-group-small' id='group-2'>
+  <div class='parallax-layer base-layer'>
+    <section>
+      <div class="container">
+        <div class="d-flex align-items-center">
+          <div class='card-parent'>
+            <div class='card-child'></div>
+          </div>  
+            <div class='div-width'>
+              <div class="d-flex align-items-center">
+                <div class="icon-circle me-3">
+                  <i class="bi bi-shop circle-icon"></i>
+                </div>
+                <h1 class='title-color'>Gestión de tiendas</h1>
+              </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia eos optio qui voluptates quasi iste perferendis veritatis sequi libero dolorem ratione quidem, aut tempore doloremque nisi. Repellendus explicabo magnam perferendis.
+            Tenetur sapiente, repellat cumque dolorum, minus quas commodi et, provident odio corporis cum architecto. Alias provident consequatur amet ipsa tenetur nam dignissimos deleniti officia, autem vitae illum, impedit dolorem adipisci?</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+<!--END FIFTH SECTION-->
+
 <!--OUTRO SECTION-->
-<div class='parallax-section outro-screen'>
-<section class='hidden'>
-  <h1>And then, something else</h1>
-</section>
+<div class='parallax-section outro-screen' id='outro'>
+  <section class='hidden'>
+    <h1>And then, something else</h1>
+  </section>
 </div>
 <!--END OUTRO SECTION-->
 
